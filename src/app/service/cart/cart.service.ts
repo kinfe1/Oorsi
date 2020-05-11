19d625938ecb78ebf5304434d6b86f2b6dd9a65b
Zemichael Tesfaye <zzimick@gmail.com>
2020-05-12 00:16:00 +0300

    update cart product fixed.

--- src/app/service/cart/cart.service.ts
+++ src/app/service/cart/cart.service.ts
@@ -1,19 +1,16 @@
-import { AuthService } from './../auth/auth.service';
-import { OORSI_API_ENDPOINT } from '../../const';
-import { CartProduct } from '../../model/cartProduct';
-import { Injectable, EventEmitter } from '@angular/core';
+import { AuthService } from "./../auth/auth.service";
+import { OORSI_API_ENDPOINT } from "../../const";
+import { CartProduct } from "../../model/cartProduct";
+import { Injectable, EventEmitter } from "@angular/core";
 
-
-import { Product } from '../../model/product';
-import { User } from '../../model/user';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
-import { HttpParams } from '@angular/common/http';
-
+import { Product } from "../../model/product";
+import { User } from "../../model/user";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs/Observable";
+import { HttpParams } from "@angular/common/http";
 
 @Injectable()
 export class CartService {
-
   cartUpdated: EventEmitter<number>;
 
   constructor(private http: HttpClient) {
@@ -21,44 +18,43 @@ export class CartService {
   }
 
   getCart(): Observable<CartProduct[]> {
-
-    return this.http.get<CartProduct[]>(OORSI_API_ENDPOINT + 'cart/products');
+    return this.http.get<CartProduct[]>(OORSI_API_ENDPOINT + "cart/products");
   }
 
   getCartSize(): Observable<number> {
-
-    return this.http.get<number>(OORSI_API_ENDPOINT + 'cart/size');
+    return this.http.get<number>(OORSI_API_ENDPOINT + "cart/size");
   }
 
   updateCartSize(): void {
-    this.getCartSize().subscribe(data => this.cartUpdated.emit(data));
+    this.getCartSize().subscribe((data) => this.cartUpdated.emit(data));
   }
 
   deleteCartProduct(cartProduct: CartProduct): Promise<any> {
-
     return new Promise<any>((resolve, reject) => {
-      this.http.post(OORSI_API_ENDPOINT + 'cart/remove',
-        {
-          to: cartProduct.forUser.userID,
-          productId: cartProduct.product.productId,
-        },
-        { responseType: 'text' }
-      ).subscribe(data => {
-        console.log('CartProvider')
-        this.updateCartSize();
-        resolve(data)
-      });
+      this.http
+        .post(
+          OORSI_API_ENDPOINT + "cart/remove",
+          {
+            to: cartProduct.forUser.userID,
+            productId: cartProduct.product.productId,
+          },
+          { responseType: "text" }
+        )
+        .subscribe((data) => {
+          console.log("CartProvider");
+          this.updateCartSize();
+          resolve(data);
+        });
     });
   }
 
   addProductToCart(product: Product, to?: User) {
-
     let cartProduct;
 
     if (null != product.productId) {
-      cartProduct = { 'productId': product.productId }
+      cartProduct = { productId: product.productId };
     } else {
-      cartProduct = { 'retailer': product.retailerId, 'sku': product.sku }
+      cartProduct = { retailer: product.retailerId, sku: product.sku };
     }
 
     if (to) {
@@ -66,19 +62,27 @@ export class CartService {
     }
 
     return new Promise<any>((resolve, reject) => {
-      this.http.post(OORSI_API_ENDPOINT + 'cart/add', cartProduct).subscribe(data => {
-        this.updateCartSize();
-        resolve(data);
-      })
+      this.http
+        .post(OORSI_API_ENDPOINT + "cart/add", cartProduct)
+        .subscribe((data) => {
+          this.updateCartSize();
+          resolve(data);
+        });
     });
   }
 
   updateCartProduct(cartProduct: CartProduct): Promise<any> {
     return new Promise<any>((resolve, reject) => {
-      this.http.post(OORSI_API_ENDPOINT + 'cart/product/update', { to: cartProduct.forUser.userID, productId: cartProduct.product.productId, quantity: cartProduct.quantity }).subscribe(data => {
-        this.updateCartSize();
-        resolve(data);
-      });
-    })
+      this.http
+        .post(OORSI_API_ENDPOINT + "cart/product/update", {
+          forId: cartProduct.forUser.userID,
+          productId: cartProduct.product.productId,
+          quantity: cartProduct.quantity,
+        })
+        .subscribe((data) => {
+          this.updateCartSize();
+          resolve(data);
+        });
+    });
   }
 }
